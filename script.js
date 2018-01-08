857a7f841dc7b4850d197ebee4cecebcac2f8f01
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2018-01-08 11:53:35 -0700

    pe

---
+++ script.js
@@ -0,0 +1,31 @@
+let questionNum = 1;
+
+$("#next").click(nextQuestion);
+$("#prev").click(prevQuestion);
+$("#mark").click(markQuiz);
+
+function nextQuestion() {
+  $("#question" + questionNum).hide();
+
+  questionNum++;
+  if (questionNum > 4) {
+    questionNum = 1;
+  }
+
+  $("#question" + questionNum).show();
+}
+
+function prevQuestion() {
+  $("#question" + questionNum).hide();
+
+  questionNum--;
+  if (questionNum < 1) {
+    questionNum = 4;
+  }
+
+  $("#question" + questionNum).show();
+}
+
+function markQuiz() {
+  let
+}
