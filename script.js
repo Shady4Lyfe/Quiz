4a522e06dee626d1b9b170acb5c1cf325e8ca97f
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2018-01-15 01:03:12 -0700

    yt

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function nextQuestion() {
   $("#question" + questionNum).hide();
 
   questionNum++;
-  if (questionNum > 4) {
+  if (questionNum > 5) {
     questionNum = 1;
   }
 
@@ -20,18 +20,47 @@ function prevQuestion() {
 
   questionNum--;
   if (questionNum < 1) {
-    questionNum = 4;
+    questionNum = 5;
   }
 
   $("#question" + questionNum).show();
 }
 
 function markQuiz() {
-  
+  let answer1 = $("input[name='option']:checked").val();
   let answer2 = $("#answer2").val();
+  let answer3 = $("#answer3").val();
+  let answer4 = $("#answer4").val();
+  let answer5 = $("#answer4").val();
+
+  if (answer1 == "C") {
+    alert("true")
+  } else {
+    alert("false")
+  }
+
   if (answer2 == "kkOma") {
+    alert("yes")
+  } else {
+    alert("no")
+  }
+
+  if (answer3 == "3") {
+    alert("correct")
+  } else {
+    alert("nein")
+  }
+
+  if (answer4 == "samsung galaxy" || answer4 == "Samsung Galaxy" || answer4 == "SSG" || answer4 == "ssg") {
+    alert(1)
+  } else {
+    alert(2)
   }
   
-  if ()
+  if (answer5 == "5") {
+    alert("correct")
+  } else {
+    alert("nein")
+  }
   
 }
